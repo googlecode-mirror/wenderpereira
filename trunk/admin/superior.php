b0efcda48a07a4c61d9ba01e6cc9b3d68378c066
wendercorrea@gmail.com <wendercorrea@gmail.com@4175976c-a43a-fb8f-40b8-59962b5d69b5>
2013-05-04 19:29:58 +0000

    Criação_da_parte_de_administração

---
+++ trunk/admin/superior.php
@@ -0,0 +1,17 @@
+<? 	@session_start(); // Inicializa a sessão
+     include "header.php";
+	 include "conexao.php";
+	 connect();
+	 	 
+?>
+
+<html>
+<head>
+<title>Funarte</title>
+<base target="conteúdo">
+</head>
+
+<body topmargin="0" bgcolor="<?  echo $cor_pagina ?>">
+
+</body>
+</html>
