82981133208496d638925e09b12c773789b37cc2
wendercorrea@gmail.com <wendercorrea@gmail.com@4175976c-a43a-fb8f-40b8-59962b5d69b5>
2013-05-06 11:04:37 +0000

    Projeto_em_produção

--- trunk/email.php
+++ trunk/email.php
@@ -1,56 +1,67 @@
-<?
-session_start();
-if (empty($_SESSION['login'])) {
-	?> <script>
-     alert("Acesso Negado!");
-	 window.location = 'index.php';
-	 </script>
-	<?php
-}else{
+<? 
+	session_start();
 	include "conexao.php";
+	include "sqlInjection.php";
 	connect();
 	//-----------------------------------------------
+	
+
+
 	$date = date("d/m/y");
 	$hora = date("H:i");
-	$Login 		= trim($_POST["login"]);
-	$Senha 		= trim($_POST["senha"]);
-	$Senha1		= trim($_POST["senha1"]);
-	$Nome		= trim($_POST["login"]);	// Pega o valor do campo Nome
-	$Email		= trim($_POST["email"]);	// Pega o valor do campo Email
+	$Login 		= anti_sql_injection($_POST["login"]);
+	$Senha 		= anti_sql_injection($_POST["senha"]);
+	$Senha1		= anti_sql_injection($_POST["senha1"]);
+	$Nome		= anti_sql_injection($_POST["login"]);	// Pega o valor do campo Nome
+	$Email		= anti_sql_injection($_POST["email"]);	// Pega o valor do campo Email
 	$Ativacao	= "http://www.funarte.gov.br/criacao_literaria/autenticaremail.php?x44221715457fghsr=$Email&1425=$Login";	
 	// Pega os valores do campo Mensagem
 	$Agradecimento = "Dúvidas: \n\n cepin@funarte.gov.br \n (21)2279-8082 ";
+	
 	if (empty($Login) OR empty($Senha) OR $Senha != $Senha1) {
 	?>
-	   <script>
-         alert("Senhas diferentes ou usuário já existe no banco de dados!");
-	     window.location = 'novoUsuario.php';
-	   </script>
+		    <script>
+               alert("Senhas diferentes ou usuário já existe no banco de dados!");
+			   window.location = 'novoUsuario.php';
+			          </script>
 	<?php
       } else 
    {
-	//-----------------------------------------------
-	//inserção
-	//-----------------------------------------------
-	$consulta = "INSERT INTO usuarios (login,senha,nome,ativo,email,data,hora)
-    VALUES 
-	('$Login','$Senha','$Nome','1','$Email','$date','$hora')";
-	$resultado = mysql_query($consulta)
-	or die ("Usuário ou senha já existem, falha ao inserir dados!");
-	//-----------------------------------------------
-	//Email
-	//-----------------------------------------------
+
+
+//-----------------------------------------------
+//inserção
+//-----------------------------------------------
+
+$consulta = "INSERT INTO usuarios (login,senha,nome,ativo,email,data,hora)
+ VALUES 
+ ('$Login','$Senha','$Nome','1','$Email','$date','$hora')";
+ 
+$resultado = mysql_query($consulta)
+or die ("Usuário ou senha já existem, falha ao inserir dados!");
+
+//-----------------------------------------------
+//Email
+//-----------------------------------------------
+
+
 	// $Nome		= $_POST["Nome"];	// Pega o valor do campo Nome
 	// $Fone		= $_POST["Fone"];	// Pega o valor do campo Telefone
 	// $Email		= $_POST["Email"];	// Pega o valor do campo Email
 	// $Mensagem	= $_POST["Mensagem"];	// Pega os valores do campo Mensagem
- 	// Variável que junta os valores acima e monta o corpo do email
-	$Vai = "$Nome,\n\n Seus dados de cadastro são \n\n E-mail: $Email \n\n Para ativar o seu login clique no link abaixo! \n
-	\n Link de ativação: $Ativacao\n \n\n $Agradecimento";
-	require_once("phpmailer/class.phpmailer.php");
-	define('GUSER', 'cadastrofunarte@gmail.com');	// <-- Insira aqui o seu GMail
-	define('GPWD', '$empreligado');		// <-- Insira aqui a senha do seu GMail
-	function smtpmailer($para, $de, $de_nome, $assunto, $corpo) { 
+
+
+ 
+ // Variável que junta os valores acima e monta o corpo do email
+$Vai = "$Nome,\n\n Seus dados de cadastro são \n\n E-mail: $Email \n\n Para ativar o seu login clique no link abaixo! \n
+		\n Link de ativação: $Ativacao\n \n\n $Agradecimento";
+
+require_once("phpmailer/class.phpmailer.php");
+
+define('GUSER', 'cadastrofunarte@gmail.com');	// <-- Insira aqui o seu GMail
+define('GPWD', '$empreligado');		// <-- Insira aqui a senha do seu GMail
+
+function smtpmailer($para, $de, $de_nome, $assunto, $corpo) { 
 	global $error;
 	$mail = new PHPMailer();
 	$mail->IsSMTP();		// Ativar SMTP
@@ -69,21 +80,28 @@ if (empty($_SESSION['login'])) {
 		$error = 'Mail error: '.$mail->ErrorInfo; 
 		return false;
 	} else {
-  	    ?>
-	  <script>
-		alert("Cadastro realizado com sucesso acesse seu email e ative o usuário!");
-		window.location = 'index.php';
-	  </script>
-		<?php
+			?>
+		    <script>
+               alert("Cadastro realizado com sucesso acesse seu email e ative o usuário!");
+			   window.location = 'index.php';
+             </script>
+			<?php
 		return true;
-		}
-	}
-	// Insira abaixo o email que irá receber a mensagem, o email que irá enviar (o mesmo da variável GUSER), 
-	//o nome do email que envia a mensagem, o Assunto da mensagem e por último a variável com o corpo do email.
-	 if (smtpmailer($Email, '$Email', 'Nome do Enviador', $Email, $Vai)) {
-	//Header("location:http://localhost:8080/"); // Redireciona para uma página de obrigado.
-	}
-	if (!empty($error)) echo $error;
 	}
 }
+
+// Insira abaixo o email que irá receber a mensagem, o email que irá enviar (o mesmo da variável GUSER), 
+//o nome do email que envia a mensagem, o Assunto da mensagem e por último a variável com o corpo do email.
+
+ if (smtpmailer($Email, '$Email', 'Nome do Enviador', $Email, $Vai)) {
+
+	//Header("location:http://localhost:8080/"); // Redireciona para uma página de obrigado.
+
+}
+if (!empty($error)) echo $error;
+
+}
+
+
+
 ?>
--- trunk/sql/parteIII.php
+++ trunk/sql/parteIII.php
@@ -1,17 +1,13 @@
+
 <?
-session_start();
-if (empty($_SESSION['login'])) {
-	?> <script>
-     alert("Acesso Negado!");
-	 window.location = 'index.php';
-	 </script>
-	<?php
-}else{
+	session_start();
 	include "conexao.php";
 	connect();
 	$Login 	=  trim($_SESSION["login"]);
 	$date = date("d/m/y");
 	$hora = date("H:i");
+	
+//	echo($Login);
 	  $questao1 = $_POST[qtd1];
 	  $questao2 = $_POST[qtd2];
   	  $questao3 = $_POST[qtd3];
@@ -21,22 +17,37 @@ if (empty($_SESSION['login'])) {
 	  $questao6 = $_POST[qtd6];
 	  $questao7Quais = $_POST[qtd7quais];
 	 // inserindo informaçoes
-	  insere($questao1,$Login,$date,$hora);
- 	  insere($questao2,$Login,$date,$hora);
- 	  insere($questao2,$Login,$date,$hora);
- 	  insere($questao3,$Login,$date,$hora);
- 	  insere($questao3Quais,$Login,$date,$hora);
- 	  insere($questao4,$Login,$date,$hora);
- 	  insere($questao5,$Login,$date,$hora);
- 	  insere($questao6,$Login,$date,$hora);
+	 
+	 if(empty($questao2)) {}else{
+	  insere($questao1,$Login,$date,$hora);}
+	 if(empty($questao2)) {}else{	  
+ 	  insere($questao2,$Login,$date,$hora);}
+	 if(empty($questao2)) {}else{	  
+ 	  insere($questao2,$Login,$date,$hora);}
+	 if(empty($questao2)) {}else{	 
+ 	  insere($questao3,$Login,$date,$hora);}
+	 if(empty($questao2)) {}else{	  
+ 	  insere($questao4,$Login,$date,$hora);}
+	 if(empty($questao2)) {}else{	  
+ 	  insere($questao5,$Login,$date,$hora);}
+	 if(empty($questao2)) {}else{	  
+ 	  insere($questao6,$Login,$date,$hora);}
+	    // inserindo se não branco
+      if(empty($questao3Quais)) {
+		  }else{
+		   $qtdQuestao = qtd3;
+		 inserequais($qtdQuestao,$questao3Quais,$Login,$date,$hora);}	   
+	    // inserindo se não branco
+      if(empty($questao7Quais)) {
+		  }else{
+		   $qtdQuestao = qtd7;
+		 inserequais($qtdQuestao,$questao7Quais,$Login,$date,$hora);}	  
    	 //----------------------------
 	  $_checkbox = $_POST['qtd7'];
 		foreach($_checkbox as $_valor){
 			insere($_valor,$Login,$date,$hora);
 	  }
 	  //----------------------------
- 	  insere($questao7Quais,$Login,$date,$hora);
-   	   
 	  //função insere---------------------------------------------  
 	function insere(&$resposta1,&$usuario,&$date,&$hora) {
 		
@@ -47,7 +58,14 @@ if (empty($_SESSION['login'])) {
 		or die (mysql_error());
 		//função insere---------------------------------------------
 	}
-}
+	  function inserequais(&$qtdQuestao,&$resposta1,&$usuario,&$date,&$hora) {
+		$consulta = "INSERT INTO pesquisaquais (questao,respostas,usuario,parte,data,hora)
+		 VALUES ('$qtdQuestao','$resposta1','$usuario','3','$date','$hora')";
+		$resultado = mysql_query($consulta)
+		or die (mysql_error());
+		//função insere---------------------------------------------
+	}
+	
 	?>
 	 <script language="JavaScript">
      alert("ParteIII cadastradra, clique na parteIV para realizar o cadastro!");
