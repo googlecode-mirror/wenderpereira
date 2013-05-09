8676039b3e30c18f5d80e935818b0757396a0b7c
wendercorrea <wendercorrea@gmail.com>
2013-05-09 18:08:48 +0000

    campo de telefone com quantidade menor de caracteres

--- js/jsValidate.js
+++ js/jsValidate.js
@@ -93,9 +93,9 @@ if (campo.value.length == 0)
 campo.value = '(' + campo.value;
 
 if (campo.value.length == 3)
-campo.value = campo.value + ') ';
+campo.value = campo.value + ')';
 
-if (campo.value.length == 9)
+if (campo.value.length == 8)
 campo.value = campo.value + '-';
 
 }
@@ -160,3 +160,59 @@ function validateEmail_(form_id,email) {
 //		$(document).ready(function(){
 //		    $("#zipcode").mask("99999-999")
 //		});
+
+
+////http://www.geradorcnpj.com/javascript-validar-cnpj.htm
+//function validarCNPJ(cnpj) {
+// 
+//    cnpj = cnpj.replace(/[^\d]+/g,'');
+// 
+//    if(cnpj == '') return false;
+//     
+//    if (cnpj.length != 14)
+//        return false;
+// 
+//    // Elimina CNPJs invalidos conhecidos
+//    if (cnpj == "00000000000000" || 
+//        cnpj == "11111111111111" || 
+//        cnpj == "22222222222222" || 
+//        cnpj == "33333333333333" || 
+//        cnpj == "44444444444444" || 
+//        cnpj == "55555555555555" || 
+//        cnpj == "66666666666666" || 
+//        cnpj == "77777777777777" || 
+//        cnpj == "88888888888888" || 
+//        cnpj == "99999999999999")
+//        return false;
+//         
+//    // Valida DVs
+//    tamanho = cnpj.length - 2
+//    numeros = cnpj.substring(0,tamanho);
+//    digitos = cnpj.substring(tamanho);
+//    soma = 0;
+//    pos = tamanho - 7;
+//    for (i = tamanho; i >= 1; i--) {
+//      soma += numeros.charAt(tamanho - i) * pos--;
+//      if (pos < 2)
+//            pos = 9;
+//    }
+//    resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+//    if (resultado != digitos.charAt(0))
+//        return false;
+//         
+//    tamanho = tamanho + 1;
+//    numeros = cnpj.substring(0,tamanho);
+//    soma = 0;
+//    pos = tamanho - 7;
+//    for (i = tamanho; i >= 1; i--) {
+//      soma += numeros.charAt(tamanho - i) * pos--;
+//      if (pos < 2)
+//            pos = 9;
+//    }
+//    resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+//    if (resultado != digitos.charAt(1))
+//          return false;
+//           
+//    return true;
+//    
+//}
