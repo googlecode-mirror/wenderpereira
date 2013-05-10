5d8998ff41b4fbba7396b53f0797ba355dfa4d54
wendercorrea@gmail.com <wendercorrea@gmail.com@4175976c-a43a-fb8f-40b8-59962b5d69b5>
2013-05-10 17:49:45 +0000

    Backupp Base_Mysql

--- trunk/admin/script_banco/criacao_literaria.sql
+++ trunk/admin/script_banco/criacao_literaria.sql
@@ -1,136 +1,243 @@
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+-- phpMyAdmin SQL Dump
+-- version 3.3.0
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: May 10, 2013 at 02:48 PM
+-- Server version: 5.1.44
+-- PHP Version: 5.2.13
 
-CREATE SCHEMA IF NOT EXISTS `criacao_literaria` DEFAULT CHARACTER SET latin1 ;
-USE `criacao_literaria` ;
-
--- -----------------------------------------------------
--- Table `criacao_literaria`.`historico`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `criacao_literaria`.`historico` (
-  `idhistorico` INT(11) NOT NULL ,
-  `usuario` VARCHAR(45) NULL DEFAULT NULL ,
-  `acao` VARCHAR(45) NULL DEFAULT NULL ,
-  PRIMARY KEY (`idhistorico`) )
-ENGINE = MyISAM
-DEFAULT CHARACTER SET = latin1;
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 
--- -----------------------------------------------------
--- Table `criacao_literaria`.`mapeamento`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `criacao_literaria`.`mapeamento` (
-  `idmapeamento` INT(11) NOT NULL AUTO_INCREMENT ,
-  `nome` VARCHAR(45) NOT NULL ,
-  `cargointituicao` VARCHAR(45) NOT NULL ,
-  `cpf` VARCHAR(45) NULL DEFAULT NULL ,
-  `email` VARCHAR(45) NULL DEFAULT NULL ,
-  `telefone` VARCHAR(45) NULL DEFAULT NULL ,
-  `usuario` VARCHAR(45) NULL DEFAULT NULL ,
-  `preenchido` CHAR(1) NULL DEFAULT NULL ,
-  `data` VARCHAR(45) NULL DEFAULT NULL ,
-  `hora` VARCHAR(45) NULL DEFAULT NULL ,
-  PRIMARY KEY (`idmapeamento`) )
-ENGINE = MyISAM
-AUTO_INCREMENT = 5
-DEFAULT CHARACTER SET = latin1
-COMMENT = '	';
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `criacao_literaria`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `historico`
+--
+
+CREATE TABLE IF NOT EXISTS `historico` (
+  `idhistorico` int(11) NOT NULL,
+  `usuario` varchar(45) DEFAULT NULL,
+  `acao` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idhistorico`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `historico`
+--
 
 
--- -----------------------------------------------------
--- Table `criacao_literaria`.`mapeamentoparteI`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `criacao_literaria`.`mapeamentoparteI` (
-  `idmapeamento` INT(11) NOT NULL AUTO_INCREMENT ,
-  `nomeintituicao` VARCHAR(45) NOT NULL ,
-  `cnpj` VARCHAR(45) NULL DEFAULT NULL ,
-  `endereco` VARCHAR(80) NOT NULL ,
-  `municipio` VARCHAR(45) NOT NULL ,
-  `unidadefederativa` VARCHAR(45) NULL DEFAULT NULL ,
-  `cep` VARCHAR(18) NOT NULL ,
-  `telefone` VARCHAR(45) NULL DEFAULT NULL ,
-  `email` VARCHAR(58) NULL DEFAULT NULL ,
-  `usuario` VARCHAR(45) NULL DEFAULT NULL ,
-  `preenchido` VARCHAR(1) NULL DEFAULT NULL ,
-  `data` VARCHAR(45) NULL DEFAULT NULL ,
-  `hora` CHAR(45) NULL DEFAULT NULL ,
-  PRIMARY KEY (`idmapeamento`) )
-ENGINE = MyISAM
-AUTO_INCREMENT = 3
-DEFAULT CHARACTER SET = latin1
-COMMENT = '	';
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mapeamento`
+--
+
+CREATE TABLE IF NOT EXISTS `mapeamento` (
+  `idmapeamento` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(45) NOT NULL,
+  `cargointituicao` varchar(45) NOT NULL,
+  `cpf` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `telefone` varchar(45) DEFAULT NULL,
+  `usuario` varchar(45) DEFAULT NULL,
+  `preenchido` char(1) DEFAULT NULL,
+  `data` varchar(45) DEFAULT NULL,
+  `hora` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idmapeamento`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COMMENT='	' AUTO_INCREMENT=20 ;
+
+--
+-- Dumping data for table `mapeamento`
+--
 
 
--- -----------------------------------------------------
--- Table `criacao_literaria`.`pesquisa`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `criacao_literaria`.`pesquisa` (
-  `idpesquisa` INT(11) NOT NULL AUTO_INCREMENT ,
-  `respostas` VARCHAR(45) NULL DEFAULT NULL ,
-  `usuario` VARCHAR(45) NULL DEFAULT NULL ,
-  `parte` VARCHAR(45) NULL DEFAULT NULL ,
-  `data` VARCHAR(45) NULL DEFAULT NULL ,
-  `hora` VARCHAR(45) NULL DEFAULT NULL ,
-  PRIMARY KEY (`idpesquisa`) )
-ENGINE = MyISAM
-AUTO_INCREMENT = 85
-DEFAULT CHARACTER SET = latin1;
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mapeamentopartei`
+--
+
+CREATE TABLE IF NOT EXISTS `mapeamentopartei` (
+  `idmapeamento` int(11) NOT NULL AUTO_INCREMENT,
+  `nomeintituicao` varchar(45) NOT NULL,
+  `cnpj` varchar(45) DEFAULT NULL,
+  `endereco` varchar(80) NOT NULL,
+  `municipio` varchar(45) NOT NULL,
+  `unidadefederativa` varchar(45) DEFAULT NULL,
+  `cep` varchar(18) NOT NULL,
+  `telefone` varchar(45) DEFAULT NULL,
+  `email` varchar(58) DEFAULT NULL,
+  `usuario` varchar(45) DEFAULT NULL,
+  `preenchido` varchar(1) DEFAULT NULL,
+  `data` varchar(45) DEFAULT NULL,
+  `hora` char(45) DEFAULT NULL,
+  PRIMARY KEY (`idmapeamento`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 COMMENT='	' AUTO_INCREMENT=84 ;
+
+--
+-- Dumping data for table `mapeamentopartei`
+--
+
+INSERT INTO `mapeamentopartei` (`idmapeamento`, `nomeintituicao`, `cnpj`, `endereco`, `municipio`, `unidadefederativa`, `cep`, `telefone`, `email`, `usuario`, `preenchido`, `data`, `hora`) VALUES
+(54, 'wender1', '12121211', 'Rua Doutor Alfredo Backer, 579 bl 01 apt 5041', 'Mutondo1', 'AC1', '24452-0011', '(21) 8220-97331', 'wendercorrea@gmail.com1', 'wender1', '1', '09/05/13', '12:43'),
+(55, 'wender', '1212121', 'Rua Doutor Alfredo Backer, 579 bl 01 apt 504', 'Mutondo', 'AC', '24452-001', '(21) 8220-9733', 'wendercorrea@gmail.com', 'wender', '1', '09/05/13', '12:43'),
+(52, 'wender', '1212121', 'Rua Doutor Alfredo Backer, 579 bl 01 apt 504', 'Mutondo', 'AC', '24452-001', '(21) 8220-9733', 'wendercorrea@gmail.com', 'wender', '1', '09/05/13', '12:43'),
+(53, 'wender', '1212121', 'Rua Doutor Alfredo Backer, 579 bl 01 apt 504', 'Mutondo', 'AC', '24452-001', '(21) 8220-9733', 'wendercorrea@gmail.com', 'wender', '1', '09/05/13', '12:43'),
+(51, 'wender', '1212121', 'Rua Doutor Alfredo Backer, 579 bl 01 apt 504', 'Mutondo', 'AC', '24452-001', '(21) 8220-9733', 'wendercorrea@gmail.com', 'wender', '1', '09/05/13', '12:42'),
+(50, 'wender', '1212121', 'Rua Doutor Alfredo Backer, 579 bl 01 apt 504', 'Mutondo', 'AC', '24452-001', '(21) 8220-9733', 'wendercorrea@gmail.com', 'wender', '1', '09/05/13', '12:42'),
+(49, 'wender', '1212121', 'Rua Doutor Alfredo Backer, 579 bl 01 apt 504', 'Mutondo', 'AC', '24452-001', '(21) 8220-9733', 'wendercorrea@gmail.com', 'wender', '1', '09/05/13', '12:33'),
+(48, 'wender', '1212121', 'Rua Doutor Alfredo Backer, 579 bl 01 apt 504', 'Mutondo', 'AC', '24452-001', '(21) 8220-9733', 'wendercorrea@gmail.com', 'wender', '1', '09/05/13', '12:33'),
+(47, 'wender', '1212121', 'Rua Doutor Alfredo Backer, 579 bl 01 apt 504', 'Mutondo', 'AC', '24452-001', '(21) 8220-9733', 'wendercorrea@gmail.com', 'wender', '1', '09/05/13', '12:32'),
+(46, 'wender', '1212121', 'Rua Doutor Alfredo Backer, 579 bl 01 apt 504', 'Mutondo', 'AC', '24452-001', '(21) 8220-9733', 'wendercorrea@gmail.com', 'wender', '1', '09/05/13', '10:28'),
+(45, 'wender', '1212121', 'Rua Doutor Alfredo Backer, 579 bl 01 apt 504', 'Mutondo', 'AC', '24452-001', '(21) 8220-9733', 'wendercorrea@gmail.com', 'wender', '1', '09/05/13', '09:28'),
+(43, 'wender', '1212121', 'Rua Doutor Alfredo Backer, 579 bl 01 apt 504', 'Mutondo', 'RJ', '24452-001', '(21) 8220-9733', 'wendercorrea@gmail.com', 'wender', '1', '08/05/13', '16:15'),
+(44, 'wender', '1212121', 'Rua Doutor Alfredo Backer, 579 bl 01 apt 504', 'Mutondo', 'AC', '24452-001', '(21) 8220-9733', 'wendercorrea@gmail.com', 'wender', '1', '09/05/13', '09:27'),
+(56, 'wender', '1212121', 'Rua Doutor Alfredo Backer, 579 bl 01 apt 504', 'Mutondo', 'AC', '24452-001', '(21) 8220-9733', 'wendercorrea@gmail.com', 'wender', '1', '09/05/13', '12:46'),
+(57, 'wender', '1212121', 'Rua Doutor Alfredo Backer, 579 bl 01 apt 504', 'Mutondo', 'AC', '24452-001', '(21) 8220-9733', 'wendercorrea@gmail.com', 'wender', '1', '09/05/13', '13:08'),
+(58, 'wender', '1212121', 'Rua Doutor Alfredo Backer, 579 bl 01 apt 504', 'Mutondo', 'AC', '24452-001', '(21) 8220-9733', 'wendercorrea@gmail.com', 'wender', '1', '09/05/13', '13:08'),
+(59, 'wender', '1212121', 'Rua Doutor Alfredo Backer, 579 bl 01 apt 504', 'Mutondo', 'AC', '24452-001', '(21) 8220-9733', 'wendercorrea@gmail.com', 'wender', '1', '09/05/13', '13:15'),
+(60, 'wender', '1212121', 'Rua Doutor Alfredo Backer, 579 bl 01 apt 504', 'Mutondo', 'AC', '24452-001', '(21) 8220-9733', 'wendercorrea@gmail.com', 'wender', '1', '09/05/13', '13:15'),
+(61, 'wender', '1212121', 'Rua Doutor Alfredo Backer, 579 bl 01 apt 504', 'Mutondo', 'AC', '24452-001', '(21) 8220-9733', 'wendercorrea@gmail.com', 'wender', '1', '09/05/13', '13:24'),
+(62, 'wender', '1212121', 'Rua Doutor Alfredo Backer, 579 bl 01 apt 504', 'Mutondo', 'AC', '24452-001', '(21) 8220-9733', 'wendercorrea@gmail.com', 'wender', '1', '09/05/13', '13:26'),
+(63, 'wender', '1212121', 'Rua Doutor Alfredo Backer, 579 bl 01 apt 504', 'Mutondo', 'AC', '24452-001', '(21) 8220-9733', 'wendercorrea@gmail.com', 'wender', '1', '09/05/13', '13:28'),
+(64, 'wender', '1212121', 'Rua Doutor Alfredo Backer, 579 bl 01 apt 504', 'Mutondo', 'AC', '24452-001', '(21) 8220-9733', 'wendercorrea@gmail.com', 'wender', '1', '09/05/13', '13:29'),
+(65, 'wender', '1212121', 'Rua Doutor Alfredo Backer, 579 bl 01 apt 504', 'Mutondo', 'AC', '24452-001', '(21) 8220-9733', 'wendercorrea@gmail.com', 'wender', '1', '09/05/13', '13:29'),
+(66, 'wender', '1212121', 'Rua Doutor Alfredo Backer, 579 bl 01 apt 504', 'Mutondo', 'AC', '24452-001', '(21) 8220-9733', 'wendercorrea@gmail.com', 'wender', '1', '09/05/13', '16:08'),
+(67, 'wender', '1212121', 'Rua Doutor Alfredo Backer, 579 bl 01 apt 504', 'Mutondo', 'AC', '24452-001', '(21) 8220-9733', 'wendercorrea@gmail.com', 'wender', '1', '09/05/13', '16:08'),
+(68, 'wender', '1212121', 'Rua Doutor Alfredo Backer, 579 bl 01 apt 504', 'Mutondo', 'AC', '24452-001', '(21) 8220-9733', 'wendercorrea@gmail.com', 'wender', '1', '09/05/13', '16:08'),
+(69, 'wender', '1212121', 'Rua Doutor Alfredo Backer, 579 bl 01 apt 504', 'Mutondo', 'AC', '24452-001', '(21) 8220-9733', 'wendercorrea@gmail.com', 'wender', '1', '09/05/13', '16:09'),
+(70, 'wender', '1212121', 'Rua Doutor Alfredo Backer, 579 bl 01 apt 504', 'Mutondo', 'AC', '24452-001', '(21) 8220-9733', 'wendercorrea@gmail.com', 'wender', '1', '09/05/13', '16:10'),
+(71, 'wender', '1212121', 'Rua Doutor Alfredo Backer, 579 bl 01 apt 504', 'Mutondo', 'AC', '24452-001', '(21) 8220-9733', 'wendercorrea@gmail.com', 'wender', '1', '09/05/13', '16:10'),
+(72, 'wender', '1212121', 'Rua Doutor Alfredo Backer, 579 bl 01 apt 504', 'Mutondo', 'AC', '24452-001', '(21) 8220-9733', 'wendercorrea@gmail.com', 'wender', '1', '09/05/13', '16:11'),
+(73, 'wender', '1212121', 'Rua Doutor Alfredo Backer, 579 bl 01 apt 504', 'Mutondo', 'AC', '24452-001', '(21) 8220-9733', 'wendercorrea@gmail.com', 'wender', '1', '09/05/13', '16:18'),
+(74, 'wender', '1212121', 'Rua Doutor Alfredo Backer, 579 bl 01 apt 504', 'Mutondo', 'AC', '24452-001', '(21) 8220-9733', 'wendercorrea@gmail.com', 'wender', '1', '09/05/13', '16:18'),
+(75, 'wender', '1212121', 'Rua Doutor Alfredo Backer, 579 bl 01 apt 504', 'Mutondo', 'AC', '24452-001', '(21) 8220-9733', 'wendercorrea@gmail.com', 'wender', '1', '09/05/13', '16:19'),
+(76, 'wender', '1212121', 'Rua Doutor Alfredo Backer, 579 bl 01 apt 504', 'Mutondo', 'AC', '24452-001', '(21) 8220-9733', 'wendercorrea@gmail.com', 'wender', '1', '09/05/13', '16:19'),
+(77, 'wender', '1212121', 'Rua Doutor Alfredo Backer, 579 bl 01 apt 504', 'Mutondo', 'AC', '24452-001', '(21) 8220-9733', 'wendercorrea@gmail.com', 'wender', '1', '10/05/13', '07:49'),
+(78, '', '1212121', 'Rua Doutor Alfredo Backer, 579 bl 01 apt 504', 'Mutondo', 'AC', '24452-001', '(21) 8220-9733', 'wendercorrea@gmail.com', 'wender', '1', '10/05/13', '08:13'),
+(79, '', '1212121', 'Rua Doutor Alfredo Backer, 579 bl 01 apt 504', 'Mutondo', 'AC', '24452-001', '(21) 8220-9733', 'wendercorrea@gmail.com', 'wender', '1', '10/05/13', '08:15'),
+(80, '', '1212121', 'Rua Doutor Alfredo Backer, 579 bl 01 apt 504', 'Mutondo', 'AC', '24452-001', '(21) 8220-9733', 'wendercorrea@gmail.com', 'wender', '1', '10/05/13', '08:15'),
+(81, '11111111111111', '1212121', 'Rua Doutor Alfredo Backer, 579 bl 01 apt 504', 'Mutondo', 'AC', '24452-001', '(21) 8220-9733', 'wendercorrea@gmail.com', 'wender', '1', '10/05/13', '08:24'),
+(82, '11111111111111', '1212121', 'Rua Doutor Alfredo Backer, 579 bl 01 apt 504', 'Mutondo', 'AC', '24452-001', '(21) 8220-9733', 'wendercorrea@gmail.com', 'wender', '1', '10/05/13', '08:26'),
+(83, '11111111111111', '1212121', 'Rua Doutor Alfredo Backer, 579 bl 01 apt 504', 'Mutondo', 'AC', '24452-001', '(21) 8220-9733', 'wendercorrea@gmail.com', 'wender', '1', '10/05/13', '08:28');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pesquisa`
+--
+
+CREATE TABLE IF NOT EXISTS `pesquisa` (
+  `idpesquisa` int(11) NOT NULL AUTO_INCREMENT,
+  `respostas` varchar(45) DEFAULT NULL,
+  `usuario` varchar(45) DEFAULT NULL,
+  `parte` varchar(45) DEFAULT NULL,
+  `data` varchar(45) DEFAULT NULL,
+  `hora` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idpesquisa`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=583985 ;
+
+--
+-- Dumping data for table `pesquisa`
+--
 
 
--- -----------------------------------------------------
--- Table `criacao_literaria`.`pesquisaquais`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `criacao_literaria`.`pesquisaquais` (
-  `idpesquisa` INT(11) NOT NULL AUTO_INCREMENT ,
-  `questao` VARCHAR(45) NULL DEFAULT NULL ,
-  `respostas` VARCHAR(255) NULL DEFAULT NULL ,
-  `usuario` VARCHAR(45) NULL DEFAULT NULL ,
-  `parte` VARCHAR(45) NULL DEFAULT NULL ,
-  `data` VARCHAR(45) NULL DEFAULT NULL ,
-  `hora` VARCHAR(45) NULL DEFAULT NULL ,
-  PRIMARY KEY (`idpesquisa`) )
-ENGINE = MyISAM
-AUTO_INCREMENT = 4
-DEFAULT CHARACTER SET = latin1;
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pesquisaquais`
+--
+
+CREATE TABLE IF NOT EXISTS `pesquisaquais` (
+  `idpesquisa` int(11) NOT NULL AUTO_INCREMENT,
+  `questao` varchar(45) DEFAULT NULL,
+  `respostas` varchar(255) DEFAULT NULL,
+  `usuario` varchar(45) DEFAULT NULL,
+  `parte` varchar(45) DEFAULT NULL,
+  `data` varchar(45) DEFAULT NULL,
+  `hora` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idpesquisa`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=19 ;
+
+--
+-- Dumping data for table `pesquisaquais`
+--
+
+INSERT INTO `pesquisaquais` (`idpesquisa`, `questao`, `respostas`, `usuario`, `parte`, `data`, `hora`) VALUES
+(7, 'qtdComentariosGerais', '          11          ', 'wender', '4', '07/05/13', '16:25'),
+(8, 'qtdComentariosGerais', '          11                    ', 'wender', '4', '07/05/13', '16:26'),
+(9, 'qtdComentariosGerais', '          11                              ', 'wender', '4', '07/05/13', '16:27'),
+(10, 'qtdComentariosGerais', 'tyes iik                      ', 'wender', '4', '08/05/13', '17:36'),
+(11, 'qtd3', ' ', 'wender', '3', '09/05/13', '12:33'),
+(12, 'qtdComentariosGerais', '           ', 'wender', '4', '09/05/13', '12:34'),
+(13, 'qtdComentariosGerais', '                     ', 'wender', '4', '09/05/13', '12:34'),
+(14, 'qtdComentariosGerais', '                               ', 'wender', '4', '09/05/13', '13:09'),
+(15, 'qtdComentariosGerais', '                                         ', 'wender', '4', '09/05/13', '13:09'),
+(16, 'qtdComentariosGerais', '                                                   ', 'wender', '4', '09/05/13', '13:24');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `respostas`
+--
+
+CREATE TABLE IF NOT EXISTS `respostas` (
+  `idperguntas` int(11) NOT NULL AUTO_INCREMENT,
+  `formulario` varchar(45) DEFAULT NULL,
+  `questoes` varchar(45) DEFAULT NULL,
+  `questaovalores` varchar(255) DEFAULT NULL,
+  UNIQUE KEY `idperguntas` (`idperguntas`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `respostas`
+--
 
 
--- -----------------------------------------------------
--- Table `criacao_literaria`.`respostas`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `criacao_literaria`.`respostas` (
-  `idperguntas` INT(11) NOT NULL AUTO_INCREMENT ,
-  `formulario` VARCHAR(45) NULL DEFAULT NULL ,
-  `questoes` VARCHAR(45) NULL DEFAULT NULL ,
-  `questaovalores` VARCHAR(255) NULL DEFAULT NULL ,
-  UNIQUE INDEX `idperguntas` (`idperguntas` ASC) )
-ENGINE = MyISAM
-DEFAULT CHARACTER SET = latin1;
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `usuarios`
+--
 
--- -----------------------------------------------------
--- Table `criacao_literaria`.`usuarios`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `criacao_literaria`.`usuarios` (
-  `idusuario` INT(16) NOT NULL AUTO_INCREMENT ,
-  `login` VARCHAR(20) NOT NULL ,
-  `senha` VARCHAR(8) NOT NULL ,
-  `nome` VARCHAR(20) NOT NULL ,
-  `ativo` CHAR(1) NULL DEFAULT NULL ,
-  `email` VARCHAR(45) NOT NULL ,
-  `data` VARCHAR(45) NULL DEFAULT NULL ,
-  `tipo` VARCHAR(6) NULL DEFAULT NULL COMMENT 'Adm = administrador\nAvan = avançado' ,
-  `hora` VARCHAR(45) NULL DEFAULT NULL ,
-  `concluido` VARCHAR(5) NULL DEFAULT '-1' ,
-  PRIMARY KEY (`idusuario`) ,
-  UNIQUE INDEX `login_UNIQUE` (`login` ASC) )
-ENGINE = MyISAM
-AUTO_INCREMENT = 9
-DEFAULT CHARACTER SET = latin1;
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `idusuario` int(16) NOT NULL AUTO_INCREMENT,
+  `login` varchar(20) NOT NULL,
+  `senha` varchar(8) NOT NULL,
+  `nome` varchar(20) NOT NULL,
+  `ativo` char(1) DEFAULT NULL,
+  `email` varchar(45) NOT NULL,
+  `data` varchar(45) DEFAULT NULL,
+  `tipo` varchar(6) DEFAULT NULL COMMENT 'Adm = administrador\nAvan = avançado',
+  `hora` varchar(45) DEFAULT NULL,
+  `concluido` varchar(5) DEFAULT '-1',
+  PRIMARY KEY (`idusuario`),
+  UNIQUE KEY `login_UNIQUE` (`login`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=19 ;
 
-USE `criacao_literaria` ;
+--
+-- Dumping data for table `usuarios`
+--
 
-
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+INSERT INTO `usuarios` (`idusuario`, `login`, `senha`, `nome`, `ativo`, `email`, `data`, `tipo`, `hora`, `concluido`) VALUES
+(9, 'wender', '123', 'wender', '0', '', NULL, 'adm', NULL, '0'),
+(10, 'wender1', '123', 'wender1', '0', '', NULL, NULL, NULL, '-1'),
+(11, 'wender4', '123', 'wender4', '1', 'wendercorrea@gmail.com', '09/05/13', NULL, '15:11', '-1'),
+(12, 'wender5', '123', 'wender5', '1', 'wendercorrea@gmail.com', '09/05/13', NULL, '15:12', '-1'),
+(13, 'wender6', '123', 'wender6', '1', 'wendercorrea@gmail.com', '09/05/13', NULL, '15:13', '-1'),
+(14, 'wender7', '123', 'wender7', '1', 'wendercorrea@gmail.com', '09/05/13', NULL, '15:15', '-1'),
+(15, 'wender8', '123', 'wender8', '1', 'wendercorrea@gmail.com', '09/05/13', NULL, '15:17', '-1'),
+(16, 'wender9', '123', 'wender9', '1', 'wendercorrea@gmail.com', '09/05/13', NULL, '15:19', '-1'),
+(17, 'wender10', '123', 'wender10', '1', 'wendercorrea@gmail.com', '09/05/13', NULL, '15:21', '-1'),
+(18, 'wender13', '', 'wender13', '1', 'wendercorrea@gmail.com', '10/05/13', NULL, '09:45', '-1');
