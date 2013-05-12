855cdd8fda0d52b4ca3f5cbb5153e0e818a10575
wendercorrea <wendercorrea@gmail.com>
2013-05-12 01:37:48 +0000

    antiSqlinjection

--- autenticaremail.php
+++ autenticaremail.php
@@ -1,10 +1,11 @@
 <? 
 session_start();
 include "conexao.php";
+include "sqlInjection.php";
 connect();
 
-$Email = ($_GET["x44221715457fghsr"]);
-$Login = ($_GET["1425"]);
+$Email = anti_sql_injection($_GET["x44221715457fghsr"]);
+$Login = anti_sql_injection($_GET["1425"]);
 
 $consulta = "UPDATE usuarios SET ativo='0' Where (login='$Login')";
  
