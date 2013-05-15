7f4b0b56910bee36527c8d921a9d4c3a6feb056d
wendercorrea@gmail.com <wendercorrea@gmail.com@4175976c-a43a-fb8f-40b8-59962b5d69b5>
2013-05-15 20:00:24 +0000

    testar_ARRAY

--- trunk/parteI.php
+++ trunk/parteI.php
@@ -47,6 +47,27 @@ return $resposta;
 }
 $meuArray = getArray($Login);
 ?>
+
+
+<? function getArray1(&$Login){
+$sql = "select * from pesquisaquais Where usuario= '$Login' and parte ='2';";
+$Resultado = mysql_query($sql) or die("Erro: " . mysql_error());
+$i=0;
+
+$resposta = array();
+while ($array_exibir = mysql_fetch_array($Resultado)) {
+	$resposta[$i] = array("questao"=>$array_exibir['questao'],"resposta"=>$array_exibir['respostas']);
+	$i++;
+}
+return $resposta;
+}
+$meuArray1 = getArray1($Login);
+print_r($meuArray1);
+
+?>
+
+
+
 <title>Funarte - Portal das Artes</title>
 
 <!--<script src="js/jsValidate.js" type="text/javascript"></script>
