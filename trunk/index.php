24dd5df5a94a196cb293128be8ddf8bb8a777238
wendercorrea@gmail.com <wendercorrea@gmail.com@4175976c-a43a-fb8f-40b8-59962b5d69b5>
2013-05-16 17:18:50 +0000

    Validação Login

--- trunk/index.php
+++ trunk/index.php
@@ -5,10 +5,10 @@
  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>FUNARTE</title>
 
-   <script src="js/jsValidate.js" type="text/javascript"></script>
-   <script src="js/jquery-1.6.2.js" type="text/javascript"></script>
-   <script src="js/jquery.validate.js" type="text/javascript"></script>
-   <script src="js/jsvalidarlogin.js" type="text/javascript"></script>
+   <script src="js/login/jsValidate.js" type="text/javascript"></script>
+   <script src="js/login/jquery-1.6.2.js" type="text/javascript"></script>
+   <script src="js/login/jquery.validate.js" type="text/javascript"></script>
+   <script src="js/login/jsvalidarlogin.js" type="text/javascript"></script>
    <script src="js/ajax.js" type="text/javascript"></script>  
    
  </head>    
