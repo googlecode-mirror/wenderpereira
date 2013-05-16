91b74ae5b9252c60df528d063c4ab250c4b1f313
wendercorrea@gmail.com <wendercorrea@gmail.com@4175976c-a43a-fb8f-40b8-59962b5d69b5>
2013-05-16 15:52:42 +0000

    correção email.php

--- trunk/autenticaremail.php
+++ trunk/autenticaremail.php
@@ -1,11 +1,10 @@
 <? 
 session_start();
 include "conexao.php";
-include "sqlInjection.php";
 connect();
 
-$Email = anti_sql_injection($_GET["x44221715457fghsr"]);
-$Login = anti_sql_injection($_GET["1425"]);
+$Email = ($_GET["x44221715457fghsr"]);
+$Login = ($_GET["1425"]);
 
 $consulta = "UPDATE usuarios SET ativo='0' Where (login='$Login')";
  
