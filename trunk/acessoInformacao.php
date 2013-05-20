2353b473bf5581962aa38ed14475dc98f4d3e00d
wendercorrea@gmail.com <wendercorrea@gmail.com@4175976c-a43a-fb8f-40b8-59962b5d69b5>
2013-05-20 20:23:38 +0000

    Menu acessoInformacao.php

---
+++ trunk/acessoInformacao.php
@@ -0,0 +1,13 @@
+<html>
+</style>
+<link href="css/barra.css" rel="stylesheet" type="text/css" />
+<div>
+<div class="barra">
+<div class="pagina"><a href="http://www.acessoainformacao.gov.br">
+<img src="images/ai.png" width="161" height="24" alt="" />
+<a href="http://www.brasil.gov.br" class=&quot;brasilgov">
+<img src="images/brasil.png" width="108" height="24" alt="" /></a></div>
+</div>
+</div>
+</html>
+
