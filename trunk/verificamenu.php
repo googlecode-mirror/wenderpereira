49854eb2c7e9a263f39f07fcc99dd3a28d619b09
wendercorrea@gmail.com <wendercorrea@gmail.com@4175976c-a43a-fb8f-40b8-59962b5d69b5>
2013-05-28 01:03:54 +0000

    menu

--- trunk/verificamenu.php
+++ trunk/verificamenu.php
@@ -5,40 +5,92 @@
 	$opcao = ($_GET["m"]);
 	$date = date("d/m/y");
 	$hora = date("H:i");
-
-		 $Login 	=  $_SESSION["login"]; //recebe a sessão do login
-		 
+	     $Login	=  $_SESSION["login"]; //recebe a sessão do login
 		 $sql = "select * from usuarios where login='$Login'";
 		 $Resultado = mysql_query($sql) or die("Erro: " . mysql_error());
 		 while ($array_exibir = mysql_fetch_array($Resultado)) 
 		 {
 		 $concluido = ($array_exibir['concluido']);	
 		 }
+	?>
 
-?>
-		<br/>
-		<br/>
-		<br/>
+<br/>
+<br/>
+<br/>
 <?
-			switch ($concluido + 1){
-				case(0):
-					echo 'opção0';
-				break;
-				case(1):	
-					echo 'opção1';
-				break;
-				case(2):	
-					echo 'opção2';
-				break;			
-				case(3):	
-					echo 'opção3';
-				break;
-				case(4):	
-					echo 'opção4';
-				break;			
-				default :
-				echo 'status inválido';
-			}
-			
+if($concluido == -1) {
+		if($opcao == -1){
+			 header ("location: inicial.php"); 
+		}elseif($opcao == 0){
+			 header ("location: parte_I.php"); 
+		}else{
+		
+		}
+}elseif ($concluido == 0) {
+		if($opcao == -1){
+			 header ("location: inicial.php"); 
+		}elseif($opcao == 0){
+			 header ("location: parte_I.php");
+		}elseif($opcao == 1){
+			 header ("location: parte_II.php");		
+		}else{
+		
+		}
+}elseif ($concluido == 1) {
+		if($opcao == -1){
+			 header ("location: inicial.php"); 
+		}elseif($opcao == 0){
+			 header ("location: parte_I.php");
+		}elseif($opcao == 1){
+			 header ("location: parte_II.php");		
+		}else{
+			 header ("location: inicial.php");
+		}
+}elseif ($concluido == 2) {
+			if($opcao == -1){
+			 header ("location: inicial.php"); 
+		}elseif($opcao == 0){
+			 header ("location: parte_I.php");
+		}elseif($opcao == 1){
+			 header ("location: parte_II.php");		
+		}elseif($opcao == 2){
+			 header ("location: parte_III.php");
+		}elseif($opcao == 3){
+			 header ("location: parte_III.php");
+		}else{
+			 header ("location: inicial.php");
+		}
+}elseif ($concluido == 3) {
+		if($opcao == -1){
+			 header ("location: inicial.php"); 
+		}elseif($opcao == 0){
+			 header ("location: parte_I.php");
+		}elseif($opcao == 1){
+			 header ("location: parte_II.php");		
+		}elseif($opcao == 2){
+			 header ("location: parte_III.php");
+		}elseif($opcao == 3){
+			 header ("location: parte_IV.php");
+		}else{
+			 header ("location: inicial.php");
+		}
+}elseif ($concluido == 4) {
+			if($opcao == -1){
+			 header ("location: inicial.php"); 
+		}elseif($opcao == 0){
+			 header ("location: parte_I.php");
+		}elseif($opcao == 1){
+			 header ("location: parte_II.php");		
+		}elseif($opcao == 2){
+			 header ("location: parte_III.php");
+		}elseif($opcao == 3){
+			 header ("location: parte_IV.php");
+		}elseif($opcao == 4){
+			echo("cadastramento concluido");
+		}else{
+			 header ("location: inicial.php");
+		}
+}else{
+			echo("verifique as informações");
+}
 ?>
-
