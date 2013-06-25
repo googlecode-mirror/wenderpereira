66fd78d8a669e91ac7e46e3bad132098cc66034f
wendercorrea <wendercorrea@gmail.com>
2013-06-25 18:27:26 +0000

    botões

--- parteII.php
+++ parteII.php
@@ -582,9 +582,11 @@
     </tr>
     <tr>
     <td align="center" class="formu2">
-   <div align="center">
-       <button type="submit" name="atualizar" value="atualizar"  class="btn btn-info"><i class="icon-refresh icon-white"></i> atualizarutton>
-    </div>
+       <div align="center">
+           <button type="submit" name="atualizar" value="atualizar"  class="btn btn-info"><i class="icon-refresh icon-white"></i> atualizar</button>
+           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+           <button type="submit" name="avancar" value="avancar"  class="btn btn-success" onkeypress="return handleEnter(this, event);"><i class="icon-arrow-right icon-white"></i> avancar</button>
+        </div>
     </td>
     </tr>
     </table>
