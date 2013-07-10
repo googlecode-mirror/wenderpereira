e7fcc7a9cfb64ffe154a7bbcf1c4322db61dfe7e
wendercorrea <wendercorrea@gmail.com>
2013-07-10 16:32:27 +0000

    acerto segurança

---
+++ erro1.php
@@ -0,0 +1,45 @@
+
+   <?
+     include "acessoInformacao.php";
+   ?> 
+
+
+<HTML>
+<HEAD>
+ <TITLE><?php echo $Title ?></TITLE>
+</HEAD>
+<BODY bgcolor="<?php echo $cor_pagina ?>">
+
+<br><br><br>
+<table width="600" align="center" border="0">
+<tr>
+<td><center><img src="images/imagem.jpg"></center></td>
+</tr>
+<tr>
+<td><center><font face="verdana" size="2"><b>Para realizar o Mapeamento é necessario ativar o javascript do seu navegador</b></font></center></td>
+</tr>
+<tr>
+<td>&nbsp;
+
+<div class="dropdown-toggle">
+    <center> <a href="http://www.funarte.gov.br/mapeamento_residencias"> Clique aqui para retornar a pagina inicial!</a> </center>
+ </div>
+	 
+</td>
+</tr>
+<tr>
+<td><center>
+</center></font></td>
+</tr>
+<tr>
+<td>
+</td>
+</tr>
+<tr>
+<td></td>
+</tr>
+</table>
+
+
+</BODY>
+</HTML>
