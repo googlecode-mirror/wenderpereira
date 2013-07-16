8895593720922287a5c0d911610bded44eff96af
wendercorrea@gmail.com <wendercorrea@gmail.com@4175976c-a43a-fb8f-40b8-59962b5d69b5>
2013-07-16 17:26:51 +0000

    grafico de Pizza

--- trunk/admin/graficos/usuarios.php
+++ trunk/admin/graficos/usuarios.php
@@ -13,33 +13,37 @@ require_once '../phplot/phplot.php';
       connect();
 
 
- $sql = "SELECT data,COUNT(data) as total FROM criacao_literaria.historico where acao='login' GROUP BY data;";
+ $sql = "SELECT hora,concluido,COUNT(concluido) as andamento FROM criacao_literaria.usuarios  WHERE idusuario > 100 GROUP BY concluido;";
 	   $Resultado = mysql_query($sql) or die("Erro: " . mysql_error());
 	   $i=0;
 	   $data = array();
 	   while ($array_exibir = mysql_fetch_array($Resultado)) {
-		$data[] = array($array_exibir['data'],$array_exibir['total']);
+		$data[] = array($array_exibir['concluido'],$array_exibir['andamento']);
 		$i++;
 	    }
 
 # Create a PHPlot object which will make an 800x400 pixel image:
-$p = new PHPlot(800, 400);
+$p = new PHPlot(800, 600);
 
 # Use TrueType fonts:
 //$p->SetDefaultTTFont('./arial.ttf');
 
 # Set the main plot title:
-$p->SetTitle('Quantidade de acessos por data');
+$p->SetTitle('Localizações dos usuarios na pesquisa');
 
 $p->SetPrecisionY(1);
 
 # Select the data array representation and store the data:
-$p->SetDataType('text-data');
+//para  grafico pizza
+$p->SetDataType('text-data-single');
+
+//$p->SetDataType('text-data');
 
 $p->SetDataValues($data);
 
+
 # Select the plot type - bar chart:
-$p->SetPlotType('linepoints');
+$p->SetPlotType('pie');
 
 # Define the data range. PHPlot can do this automatically, but not as well.
 //$p->SetPlotAreaWorld(0, 0, 9, 400);
@@ -50,16 +54,18 @@ $p->SetPlotType('linepoints');
 //$p->SetPlotBgColor('#ffffff');
 
 # Draw lines on all 4 sides of the plot:
-$p->SetPlotBorderType('full');
+$p->SetImageBorderType('plain');
 
 # Set a 3 line legend, and position it in the upper left corner:
-//$p->SetLegend(array('Acessos'));
+ $p->SetLegend(array('-1=cadastrado','0=Resposavel pelo Preenchimento','1=Identificação da instituição','2=Programas','3=Artistas','4=Projetos'));
 //$p->SetLegendWorld(0.1, 95);
 
 # Turn data labels on, and all ticks and tick labels off:
 //$p->SetXDataLabelPos('plotdown');
- $p->SetXLabelFontSize(2);
- $p->SetAxisFontSize(2);
+ //$p->SetXLabelFontSize(2);
+ //$p->SetAxisFontSize(2);
+ //legenda do lado esquerdo
+ //$p->SetLegendPixels(5, 5);
 
  $p->SetXTickPos('none');
 //$p->SetXTickLabelPos('none');
@@ -70,3 +76,5 @@ $p->SetYDataLabelPos('plotin');
 # Generate and output the graph now:
 $p->DrawGraph();
 ?>
+
+
