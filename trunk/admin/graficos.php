4e4c8413a81606c483d04c5a6f5fea0e8cc67679
wendercorrea@gmail.com <wendercorrea@gmail.com@4175976c-a43a-fb8f-40b8-59962b5d69b5>
2013-07-31 10:41:31 +0000

    banco 31/07/2013 produção e graficos

--- trunk/admin/graficos.php
+++ trunk/admin/graficos.php
@@ -1,12 +1,20 @@
+
+<head>
+<meta http-equiv="refresh" content="35;URL=http://www.funarte.gov.br/mapeamento_residencias/admin/graficos.php"> 
+</head>
 <fieldset>
 		<img src="graficos/acessos.php" alt="Quantidade de acessos por dia" title="Acessos site " />
 </fieldset> 
+<fieldset>
+		<img src="graficos/visualizacaoindex.php" alt="Quantidade de visualização pagina inicial por hora a partir 24/07/2013" title="Quantidade de visualização pagina inicial por hora a partir 24/07/2013" />
+</fieldset> 
+
 <fieldset>
 		<img src="graficos/loginporhora.php" alt="Quantidade de logins por hora" title="logins site " />
 </fieldset> 
 
 <fieldset>
-		<img src="graficos/horario.php" alt="Utilização do sisyema" title="Utilização do sistema" />
+		<img src="graficos/horario.php" alt="Utilização do sistema" title="Utilização do sistema" />
 </fieldset> 
 <fieldset>
 		<img src="graficos/usuarios.php" alt="Quantidade de usuarios" title="Qtd usuarios" />
